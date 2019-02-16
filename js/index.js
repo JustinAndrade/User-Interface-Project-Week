862626c26c4620da3b5f8660dc4a3fafab511ca2
JustinAndrade <j.andrade.john@gmail.com>
2019-02-16 14:33:33 -0500

    nav js complete

--- js/index.js
+++ js/index.js
@@ -1,1 +1,9 @@
-// JS goes here
+const toggleMenu = () => {
+    menu.classList.toggle('menu--open')
+};
+
+const menu = document.querySelector('.menu');
+
+const menuButton = document.querySelector('.menu-button');
+
+menuButton.addEventListener('click', toggleMenu);
