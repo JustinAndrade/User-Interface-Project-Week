cf3a83c6e8f9e83e9150edf98db89f18202b6e53
JustinAndrade <j.andrade.john@gmail.com>
2019-02-18 23:37:30 -0500

    bug fixes

--- js/index.js
+++ js/index.js
@@ -1,9 +1,10 @@
 const toggleMenu = () => {
     menu.classList.toggle('menu--open');
+    console.log('test');
 };
 
 const menu = document.querySelector('.menu');
 
-const menuButton = document.querySelector('.menu-button');
+const menuButton = document.querySelector('.open-menu-button');
 
 menuButton.addEventListener('click', toggleMenu);
