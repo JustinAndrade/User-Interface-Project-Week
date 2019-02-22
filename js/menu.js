ead86334c6c208edf2579c470935e33a31682e7b
JustinAndrade <j.andrade.john@gmail.com>
2019-02-21 23:00:39 -0500

    mobile design completed

--- js/menu.js
+++ js/menu.js
@@ -1,14 +1,27 @@
 let openMenu = document.getElementById('#')
 
 const toggleMenu = () => {
-    menu.classList.toggle('menu-open');
+    menu.classList.toggle('menu-open')
 
     console.log('test');
 };
 
+function changeImage() {
+    console.log(document.getElementById('menu-button').src);
+    
+    if (document.getElementById('menu-button').src.includes("img/nav-hamburger.png"))
+    {   
+        console.log('its the burger');
+        document.getElementById('menu-button').src = "img/nav-hamburger-close.png";
+    } else {
+        console.log('its not the burger');
+        document.getElementById('menu-button').src = "img/nav-hamburger.png"
+    }
+}
+
 const menuOpen = document.querySelector('.menu-open')
 
-const menu = document.querySelector('.menu');
+const menu = document.querySelector('.drop-menu');
 
 const menuButton = document.querySelector('#menu-button');
 
--- js/services.js
+++ js/services.js
@@ -20,12 +20,10 @@ class TabLink {
 
   select() {
     // Get all of the elements with the tabs-link class
-    const links = this.element.querySelectorAll('.tabs-link');
+    const links = document.querySelectorAll('.tabs-link');
 
     // Using a loop or the forEach method remove the 'tabs-link-selected' class from all of the links
-    Array.from(links).forEach(element  => {    
-      element.classList.remove('tabs-link-selected');
-    });
+    links.forEach(element => element.classList.remove('tabs-link-selected'));
 
     // Add a class named "tabs-link-selected" to this link
     this.element.classList.add('tabs-link-selected');
@@ -48,11 +46,11 @@ class TabItem {
     const items = document.querySelectorAll('.tabs-item')
     // Remove the class "tabs-item-selected" from each element
     items.forEach(item => item.classList.remove('tabs-item-selected'));  
-    console.log(this.items);
+    // console.log(this.items);
     // Add a class named "tabs-item-selected" to this element
     //this.element;
     this.element.classList.add('tabs-item-selected');
-    console.log(this.element);
+    // console.log(this.element);
   }
 }
 
